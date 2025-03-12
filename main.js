368fd5dc33dcd778fd0637d83b2888d8d264ec9c
Liam Reeves <wesleyliam005@gmail.com>
2025-03-12 03:55:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function printMe(){
+    console.log("I am a fucntion");
+}
+printMe(); // I am a function
+
+function printThis(param){
+    console.log(param);
+}
+
+printThis('Liam');
+
+
+//Fucntion Expression
+const stock = function(item1, item2){
+    console.log(item1, item2);
+}
+
+stock("NVIDIA RTX 5090", "AMD Ryzen 9 5950X");
+
+function sum(a,b){
+
+    let ret= a+b;
+    return ret;
+}
+
+function calc(a,b){
+    
+    return (2*(a+b));
+    console.log(a,b);
+}
+
+calc(50,60);
